07171def13c4201f1a29916f20371ae680b3148b
deeptadeeproy <deeptadeeproy@yahoo.in>
2025-10-10 21:38:24 +0530

    add binary search

---
+++ src/BinarySearch.java
@@ -0,0 +1,37 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int target = 6;
+        int[] sortedArray = { 1, 3, 6, 10, 19, 32 };
+
+        int start = 0;
+        int end = sortedArray.length - 1;
+        
+        System.out.println(findMe(target, sortedArray, start, end));
+    }
+
+    private static String findMe(int targetNumber, int[] sortedArray, int startIndex, int endIndex) {
+
+        if (startIndex > endIndex) {
+            return String.valueOf(-1);
+        }
+        if (startIndex == endIndex) {
+            if (sortedArray[startIndex] == targetNumber) {
+                return ("Found " + targetNumber + " at index " + startIndex + ".");
+            }
+            return String.valueOf(-1);
+        }
+
+        int middleIndex = (int) Math.floor((startIndex + endIndex) / 2);
+
+        if (sortedArray[middleIndex] == targetNumber) {
+            return ("Found " + targetNumber + " at index " + middleIndex + ".");
+        }
+        if (sortedArray[middleIndex] > targetNumber) {
+            findMe(targetNumber, sortedArray, startIndex, middleIndex - 1);
+        }
+        if (sortedArray[middleIndex] < targetNumber) {
+            findMe(targetNumber, sortedArray, middleIndex + 1, endIndex);
+        }
+        return String.valueOf(-1);
+    }
+}
