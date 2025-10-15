38e8c420adcb6bab474cef6ffadf62cdd1746148
deeptadeeproy <deeptadeeproy@yahoo.in>
2025-10-15 21:36:13 +0530

    optimize peakIndexInMountainArray

--- src/FindPeakIndexMountainArray.java
+++ src/FindPeakIndexMountainArray.java
@@ -8,24 +8,23 @@ public class FindPeakIndexMountainArray {
         while (start <= end) {
             int mid = (start + end) >>> 1;
             int potentialAnswer = mountainArray[mid];
-            int rightToPa = mountainArray[mid+1];
-            int leftToPa = mountainArray[mid-1];
+            int rightToPa = mountainArray[mid + 1];
+            int leftToPa = mountainArray[mid - 1];
 
             if (potentialAnswer > rightToPa && potentialAnswer > leftToPa) {
                 return mid;
-            }
-            else if (potentialAnswer > rightToPa && potentialAnswer < leftToPa) {
-                end = mid-1;
-            }
-            else if (potentialAnswer < rightToPa && potentialAnswer > leftToPa) {
-                start = mid+1;
+            } else if (potentialAnswer > rightToPa && potentialAnswer < leftToPa) {
+                end = mid - 1;
+            } else if (potentialAnswer < rightToPa && potentialAnswer > leftToPa) {
+                start = mid + 1;
             }
         }
-
-        return start;
+        return -1;
     }
+
     public static void main(String[] args) {
-        int[] mountainArray = {1,3,4,5,22,7,6,4,2};
-        System.out.println(findPeakNumber(mountainArray));
+        int[] mountainArray = { 1, 3, 4, 5, 22, 7, 6, 4, 2 };
+        int peakIndex = findPeakNumber(mountainArray);
+        System.out.println("Peak index is " + peakIndex + " and the peak number is " + mountainArray[peakIndex]);
     }
 }
